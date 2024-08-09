b60191649455e829714a1d40f02a3d5c8400c349
ABHI <abhi202864@gmail.com>
2024-08-09 21:13:04 +0530

    show connect-flash on site

--- app.js
+++ app.js
@@ -6,15 +6,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const flash = require("connect-flash");
 
-
-const sessionOptions = {
-    secret: "mysupersecretstring",
-    resave: false,
-    saveUninitialized: true
-}
-
-app.use(session(sessionOptions));
 
 // Express Routes <- require 
 const listings = require("./routes/listing.js");
@@ -29,6 +22,7 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
 
+// DB Connection
 main().then(res => console.log("connected to DB"))
     .catch(err => console.log(err));
 
@@ -37,9 +31,30 @@ async function main() {
 }
 
 
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    }
+}
+
+
 // home route
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
+});
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
 });
 
 // Express Routesr
