95b523570bc267f9b6bfb7571ea343dc70fa4aba
ABHI <abhi202864@gmail.com>
2024-08-11 23:03:16 +0530

    add register authentication

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local")
+const User = require("./models/user.js");
 
 
 // Express Routes <- require 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 
 app.set("view engine", "ejs");
@@ -30,7 +34,7 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
-
+// for session
 const sessionOptions = {
     secret: "mysupersecretcode",
     resave: false,
@@ -51,15 +55,37 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 });
 
-// Express Routesr
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+// // user registration rout
+// app.get("/demouser", async (req, res) => {
+//     let fackUser = new User({
+//         email: "student@gmail.com",
+//         username: "delta-student",
+//     });
+
+//     let registeredUser = await User.register(fackUser, "helloworld");
+//     res.send(registeredUser);
+// });
+
+
+// Express Router
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
+
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
