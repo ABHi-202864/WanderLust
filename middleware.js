55cd8cb7204bbd4ce07ebfb10fbb63c6b24cd0a9
ABHI <abhi202864@gmail.com>
2024-08-14 06:53:14 +0530

    complete Authorization

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const Listing = require("./models/listing");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 
 // server side validation
@@ -46,6 +47,16 @@ module.exports.isOwner = async (req, res, next) => {
     let listing = await Listing.findById(id);
     if (!listing.owner._id.equals(res.locals.currUser._id)) {
         req.flash("error", "You are not the Owner of this Listing .");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    let { id, reviewId } = req.params;
+    let review = await Review.findById(reviewId);
+    if (!review.author.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not the Author of this Listing .");
         return res.redirect(`/listings/${id}`);
     }
     next();
