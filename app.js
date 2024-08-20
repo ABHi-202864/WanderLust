6d83f715a637ba1eb706deaa151bdabc55e075f6
ABHI <abhi202864@gmail.com>
2024-08-20 14:06:13 +0530

    delete CLASSROOM Dir

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local")
@@ -30,17 +31,32 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
 
+const dbUrl = process.env.ATLASDB_URL;
+
 // DB Connection
 main().then(res => console.log("connected to DB"))
     .catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect(dbUrl);
 }
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+    console.log("ERROR in MONGO SESSION STORE", error);
+});
 
 // for session
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -50,11 +66,11 @@ const sessionOptions = {
     }
 }
 
-
 // home route
 // app.get("/listings", (req, res) => {
 //     res.render("listings/index");
 // });
+
 
 
 app.use(session(sessionOptions));
